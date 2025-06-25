2d50cffdef55f3760f88126f9166c78b641ddc08
Mark Dell <madelk@gmail.com>
2025-06-25 10:31:12 +0100

    feat: remove NxWelcome component and display homepage text directly in HomeView

--- apps/vue/src/views/HomeView.vue
+++ apps/vue/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import NxWelcome from '../app/NxWelcome.vue';
+import {getHomepageText} from '@study/pagetext';
+const text = getHomepageText()
 </script>
-
 <template>
   <main>
-    <NxWelcome title="@study/vue" />
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      {{ text.title }}
+    </h1>
+    <p
+      v-for="(p, i) in text.paragraphs"
+      :key="i"
+    >
+      {{ p }}
+    </p>
   </main>
 </template>
