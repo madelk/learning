44226886ab748290c95cd87ab593316ee436934e
Mark Dell <madelk@gmail.com>
2025-06-26 12:51:40 +0100

    feat: add calculator view and update router configuration

---
+++ apps/vue/src/views/CalculatorView.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="calculator">
+    <h1>This is an calculator page</h1>
+    <input id="result" class="border border-gray-300 rounded p-2 mb-4 w-full" type="text" disabled></input>
+    <div class="grid grid-cols-4 gap-2">
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('7')">
+        7
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('8')">
+        8
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('9')">
+        9
+      </button>
+      <button class="bg-red-500 text-white p-4 rounded" @click="clearResult">
+        C
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('4')">
+        4
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('5')">
+        5
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('6')">
+        6
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('/')">
+        /
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('1')">
+        1
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('2')">
+        2
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('3')">
+        3
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('*')">
+        *
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('0')">
+        0
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('.')">
+        .
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="calculateResult">
+        =
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('-')">
+        -
+      </button>
+      <button class="bg-blue-500 text-white p-4 rounded" @click="appendToResult('+')">
+        +
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+const appendToResult = (value) => {
+  const resultInput = document.getElementById('result');
+  resultInput.value += value;
+};
+</script>
+
+<style>
+@media (min-width: 768px) {
+  .calculator {
+    max-width: 768px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 1rem;
+  }
+}
+</style>
