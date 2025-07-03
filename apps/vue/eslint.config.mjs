81d230580115c0f361642e5a146b03c4e84907ce
Mark Dell <madelk@gmail.com>
2025-07-03 15:28:12 +0100

    Refactor ESLint configuration to replace vue-prettier config with eslint-config-prettier

--- apps/vue/eslint.config.mjs
+++ apps/vue/eslint.config.mjs
@@ -1,11 +1,10 @@
 import vue from 'eslint-plugin-vue';
-import vuePrettierConfig from '@vue/eslint-config-prettier';
+import prettier from 'eslint-config-prettier';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
   ...baseConfig,
   ...vue.configs['flat/recommended'],
-  ...vuePrettierConfig,
   {
     files: ['**/*.vue'],
     languageOptions: {
@@ -22,5 +21,6 @@ export default [
     rules: {
       'vue/multi-word-component-names': 'off'
     }
-  }
+  },
+  prettier
 ];
