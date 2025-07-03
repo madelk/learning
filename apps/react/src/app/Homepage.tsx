4d4009d538120d654066c4404b58c069ff5f9cee
Mark Dell <madelk@gmail.com>
2025-07-03 19:41:23 +0100

    Refine dependency management in nx.json for build and typecheck tasks

--- apps/react/src/app/Homepage.tsx
+++ apps/react/src/app/Homepage.tsx
@@ -1,11 +1,10 @@
-
 import {getHomepageText} from '@study/pagetext';
 const text = getHomepageText()
 
 export function Homepage() {
   return (<>
     <h1 className={text.title.className}>{ text.title.text }</h1>
-    {text.paragraphs.text.map((paragraph, index) => (
+    {text.paragraphs.text.map((paragraph: string, index: number) => (
       <p 
       className={text.paragraphs.className}
        key={index}>{paragraph}</p>
