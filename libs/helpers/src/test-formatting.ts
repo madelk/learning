b22d5af4261eff1b17035d38bf403952d2a680fb
Mark Dell <madelk@gmail.com>
2025-07-04 16:56:35 +0100

    ✨ SUCCESS: Auto-formatting works! This file was auto-formatted by the pre-commit hook

---
+++ libs/helpers/src/test-formatting.ts
@@ -0,0 +1,6 @@
+// This file demonstrates auto-formatting in action (will pass lint)
+const badly_formatted = (name: string): string => {
+  return `Hello ${name}!`;
+};
+
+export { badly_formatted };
---
+++ test-formatting.js
@@ -0,0 +1,6 @@
+// This file has intentional formatting issues but no lint errors
+const test = () => {
+  return "hello";
+};
+
+export { test };
