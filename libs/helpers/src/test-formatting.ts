02a5228b5a4b5fcd076278ffdf55d6ff7fbf2049
Mark Dell <madelk@gmail.com>
2025-07-04 17:00:40 +0100

    refactor: remove test file for auto-formatting demonstration

--- libs/helpers/src/test-formatting.ts
+++
@@ -1,6 +0,0 @@
-// This file demonstrates auto-formatting in action (will pass lint)
-const badly_formatted = (name: string): string => {
-  return `Hello ${name}!`;
-};
-
-export { badly_formatted };
