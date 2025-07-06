42d1ac1b249a435159eeaa8c3dbca38353153605
Mark Dell <madelk@gmail.com>
2025-07-06 13:56:08 +0100

    feat: simplify VolumeExample.vue by removing unnecessary script and dynamic content

--- apps/vue/src/views/VolumeExample.vue
+++ apps/vue/src/views/VolumeExample.vue
@@ -1,19 +1,5 @@
-<script setup lang="ts">
-  import { getHomepageText } from "@study/pagetext";
-  const text = getHomepageText();
-</script>
-
 <template>
   <main>
-    <h1 :class="text.title.className">
-      {{ text.title.text }}
-    </h1>
-    <p
-      v-for="(p, i) in text.paragraphs.text"
-      :key="i"
-      :class="text.paragraphs.className"
-    >
-      {{ p }}
-    </p>
+    <h1 class="text-3xl font-bold underline text-center mt-10">Volume</h1>
   </main>
 </template>
