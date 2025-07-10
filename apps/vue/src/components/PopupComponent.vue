7f9cd267c2a8a05eff2f5aebb3a3ccf3d56aaa38
Mark Dell <madelk@gmail.com>
2025-07-10 12:08:38 +0100

    fix: update PopupComponent integration to use show-popup prop and improve structure

--- apps/vue/src/components/PopupComponent.vue
+++ apps/vue/src/components/PopupComponent.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
   import { ref } from "vue";
   import InputComponent from "./InputComponent.vue";
+  const { showPopup } = defineProps<{
+    showPopup: boolean;
+  }>();
   //   defineEmits<(e: "closePopup", payload: string) => void>();
   defineEmits({
     closePopup: (payload: string) => {
@@ -14,40 +17,42 @@
 </script>
 
 <template>
-  <div
-    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50"
-  >
+  <teleport v-if="showPopup" to="#portal-root">
     <div
-      class="bg-mantle rounded-lg shadow-2xl p-8 w-full max-w-sm border-2 border-pink relative"
+      class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50"
     >
-      <h2
-        class="text-xl font-bold mb-4 text-mauve bg-gradient-to-r from-pink via-mauve to-lavender bg-clip-text text-transparent animate-gradient-x gradient-bg-size"
+      <div
+        class="bg-mantle rounded-lg shadow-2xl p-8 w-full max-w-sm border-2 border-pink relative"
       >
-        This is a popup
-      </h2>
-      <InputComponent
-        id="user-input"
-        v-model="userInput"
-        placeholder="Popup input placeholder"
-        label="Popup Input"
-        class="mb-4"
-      />
+        <h2
+          class="text-xl font-bold mb-4 text-mauve bg-gradient-to-r from-pink via-mauve to-lavender bg-clip-text text-transparent animate-gradient-x gradient-bg-size"
+        >
+          This is a popup
+        </h2>
+        <InputComponent
+          id="user-input"
+          v-model="userInput"
+          placeholder="Popup input placeholder"
+          label="Popup Input"
+          class="mb-4"
+        />
 
-      <button
-        class="w-full font-bold py-2 px-4 rounded transition-all duration-300 text-xl drop-shadow-lg border-2 border-blue hover:border-pink focus:outline-none animate-bg-gradient-x text-base bg-gradient-to-r from-blue via-pink to-mauve bg-[length:200%_200%] bg-[position:0%_50%]"
-        @click="$emit('closePopup', userInput)"
-      >
-        Close Popup
-      </button>
-      <button
-        class="absolute top-2 right-2 bg-gradient-to-r from-blue via-pink to-mauve bg-clip-text text-transparent text-xl font-bold focus:outline-none animate-gradient-x drop-shadow-lg"
-        aria-label="Close"
-        @click="$emit('closePopup', userInput)"
-      >
-        &times;
-      </button>
+        <button
+          class="w-full font-bold py-2 px-4 rounded transition-all duration-300 text-xl drop-shadow-lg border-2 border-blue hover:border-pink focus:outline-none animate-bg-gradient-x text-base bg-gradient-to-r from-blue via-pink to-mauve bg-[length:200%_200%] bg-[position:0%_50%]"
+          @click="$emit('closePopup', userInput)"
+        >
+          Close Popup
+        </button>
+        <button
+          class="absolute top-2 right-2 bg-gradient-to-r from-blue via-pink to-mauve bg-clip-text text-transparent text-xl font-bold focus:outline-none animate-gradient-x drop-shadow-lg"
+          aria-label="Close"
+          @click="$emit('closePopup', userInput)"
+        >
+          &times;
+        </button>
+      </div>
     </div>
-  </div>
+  </teleport>
 </template>
 
 <style scoped>
